f05e8548e291989790642e9ca7884060f481e3c5
vineetsingha207-eng <vineetsingha207@gmail.com>
2026-08-02 20:32:22 +0530

    Add files via upload

---
+++ Assignment 2/Student.java
@@ -0,0 +1,35 @@
+class Student {
+    String name;
+    int rollNo;
+
+    // Default constructor
+    Student() {
+        name = "Unknown";
+        rollNo = 0;
+    }
+
+    // Parameterized constructor
+    Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    void display() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        Student s2 = new Student("Vineet", 101);
+
+        s1.display();
+
+        System.out.println();
+
+        s2.display();
+    }
+}
+
+
